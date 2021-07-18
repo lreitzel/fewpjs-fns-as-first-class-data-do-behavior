9a247be13431227946e5a5b6ff215cae6e037dd6
Lydia <lreitzel@gmail.com>
2021-07-18 17:18:10 -0400

    commit tree 86bc3c07d8cd61ad04be6d3c9d287c3254cc8ec3

--- index.js
+++ index.js
@@ -11,3 +11,14 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(time) {
+  const hour = parseInt(time, 10);
+  if (hour < 12) return "Good Morning"
+  if (hour > 17) return "Good Evening"
+  return "Good Afternoon"
+}
+
+function displayMessage(message) {
+  document.getElementById("greeting").textContent = message;
+}
